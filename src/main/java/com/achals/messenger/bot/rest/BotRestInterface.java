108b56eca949a98bec9da736a2f02faf90e0b65c
Achal Shah <achals@gmail.com>
2016-04-19 22:46:48 -0700

    Add validation token handling

--- src/main/java/com/achals/messenger/bot/rest/BotRestInterface.java
+++ src/main/java/com/achals/messenger/bot/rest/BotRestInterface.java
@@ -4,22 +4,56 @@ package com.achals.messenger.bot.rest;
  * Created by achalshah on 4/17/16.
  */
 
-import com.achals.messenger.bot.model.Message;
+import com.google.common.base.Splitter;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.util.Map;
 
 @Path("/pin-bot/v1")
-@Produces(MediaType.APPLICATION_JSON)
+@Produces(MediaType.TEXT_PLAIN)
 public class BotRestInterface
 {
+    final String validationToken = "bd4e2912-35e2-41fc-a15f-7df1fce6c131";
+
     @GET
     @Path("health")
-    public Message health()
+    public String health()
     {
-        return new Message("healthy");
+        return "healthy";
+    }
+
+
+    @GET
+    @Path("webhook")
+    public String webhook_get(@Context HttpServletRequest request)
+    {
+        final Splitter.MapSplitter splitter = Splitter.on("&")
+                .trimResults()
+                .withKeyValueSeparator("=");
+        final String queryString = request.getQueryString();
+        if (queryString != null)
+        {
+            final Map<String, String> queryParams = splitter.split(queryString);
+
+            if (queryParams.get("hub.verify_token").equals(this.validationToken))
+            {
+                return queryParams.get("hub.challenge");
+            }
+        }
+        return "Error, wrong validation token";
+    }
+
+    @POST
+    @Path("webhook")
+    public String webhook_post(@Context HttpServletRequest request)
+    {
+        return "webhook_post";
     }
 
 }
