dd75e8c3d180333a21cf64a5037e90ec3914243f
Achal Shah <achals@gmail.com>
2016-04-19 23:04:59 -0700

    Flesh out the POST handling a bit

--- src/main/java/com/achals/messenger/bot/rest/BotRestInterface.java
+++ src/main/java/com/achals/messenger/bot/rest/BotRestInterface.java
@@ -5,6 +5,8 @@ package com.achals.messenger.bot.rest;
  */
 
 import com.google.common.base.Splitter;
+import com.google.common.io.CharStreams;
+import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
@@ -13,16 +15,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Map;
 
 @Path("/pin-bot/v1")
-@Produces(MediaType.TEXT_PLAIN)
 public class BotRestInterface
 {
     final String validationToken = "bd4e2912-35e2-41fc-a15f-7df1fce6c131";
-
+    final String accessToken = "CAAORld2IHuMBACdylvThBuZCzKfZBKMmPaLakJGXmYN5LyufuRdH7YpsQhOpayMusk6kVYlCVKoOpZCpnvF2EZActnaMrlI0KkjGSwoUAikcd9dVYHf7yz2ZBKI6ZCOfZAWN8L19cjNj2hB2fVqZAlv7FUEoHZCcK5SaIWiEyNq7rG6CLez7PgfAKSPABdZA4oto8ZD";
     @GET
     @Path("health")
+    @Produces(MediaType.TEXT_PLAIN)
     public String health()
     {
         return "healthy";
@@ -31,6 +35,7 @@ public class BotRestInterface
 
     @GET
     @Path("webhook")
+    @Produces(MediaType.TEXT_PLAIN)
     public String webhook_get(@Context HttpServletRequest request)
     {
         final Splitter.MapSplitter splitter = Splitter.on("&")
@@ -51,9 +56,21 @@ public class BotRestInterface
 
     @POST
     @Path("webhook")
+    @Produces(MediaType.APPLICATION_JSON)
     public String webhook_post(@Context HttpServletRequest request)
     {
-        return "webhook_post";
+        final String postData;
+        try(final InputStreamReader streamReader = new InputStreamReader(request.getInputStream()))
+        {
+            postData = CharStreams.toString(streamReader);
+            System.out.println(postData);
+        }
+        catch (final IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return "Error";
     }
 
 }
